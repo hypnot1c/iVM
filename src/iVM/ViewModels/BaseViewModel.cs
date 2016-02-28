51de6aaa8d4837c84c42499c9d40fe30ba1b90dc
hypnotic <ahmed_@mail.ru>
2016-02-29 00:50:52 +0300

    Caliburn app
    Convert to Caliburn application

---
+++ src/iVM/ViewModels/BaseViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iVM.ViewModels
+{
+  public class BaseViewModel: Screen
+  {
+    protected readonly INavigationService PageNavigationService;
+
+    protected BaseViewModel(INavigationService pageNavigationService)
+    {
+      PageNavigationService = pageNavigationService;
+    }
+
+    public bool CanGoBack
+    {
+      get { return PageNavigationService.CanGoBack; }
+    }
+
+    protected void NavigateTo<T>()
+    {
+      PageNavigationService.Navigate<T>();
+    }
+
+    public void GoBack()
+    {
+      PageNavigationService.GoBack();
+    }
+  }
+}
---
+++ src/iVM/ViewModels/HomeViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iVM.ViewModels
+{
+  public class HomeViewModel : BaseViewModel
+  {
+    private INavigationService _pageNavigationService;
+
+    public HomeViewModel(INavigationService pageNavigationService) : base(pageNavigationService)
+    {
+      _pageNavigationService = pageNavigationService;
+    }
+
+    private string _myMessage;
+    public string MyMessage
+    {
+      get { return _myMessage; }
+      set
+      {
+        _myMessage = value;
+        NotifyOfPropertyChange(() => MyMessage);
+      }
+    }
+
+    public void SayHello()
+    {
+      MyMessage = "Hello World!";
+    }
+  }
+}
